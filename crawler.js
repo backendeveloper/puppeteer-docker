0bc35ac1ce76cd171334f4613047c61783c4a492
Kaan Uygur <kaan@evtiko.com>
2019-03-13 10:35:56 +0300

    changed

--- crawler.js
+++ crawler.js
@@ -3,32 +3,36 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-  });
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  await page.goto('https://www.sahibinden.com/ilan/emlak-konut-satilik-b.duzu-metrobuse-5dk-125m2-tertemiz-daire-iskanli-yeni-yapi-654470042/detay', { waitUntil: 'networkidle2' });
+    await page.goto('https://www.sahibinden.com/ilan/emlak-konut-satilik-b.duzu-metrobuse-5dk-125m2-tertemiz-daire-iskanli-yeni-yapi-654470042/detay', { waitUntil: 'networkidle2' });
 
-  const links = await page.evaluate(() => {
-    const featureArticle = document
-      .evaluate(
-        '//*[@id="classifiedDetail"]/div[1]/div[1]/h1',
-        document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-      )
-      .singleNodeValue;
+    const links = await page.evaluate(() => {
+        const featureArticle = document
+            .evaluate(
+                '//*[@id="classifiedDetail"]/div[1]/div[1]/h1',
+                document,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            )
+            .singleNodeValue;
 
-    return featureArticle.textContent;
-  });
-  console.log(links);
+        if (featureArticle == null) {
+            return 'Blocked!';
+        }
 
-  browser.close();
+        return featureArticle.textContent;
+    });
+    console.log(links);
+
+    browser.close();
 
 })();
