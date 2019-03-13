ec37f76576aa563f442cb614cde64ddd5fe76569
Kaan Uygur <kaan@evtiko.com>
2019-03-13 10:34:43 +0300

    add crawler task

---
+++ crawler.js
@@ -0,0 +1,34 @@
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://www.sahibinden.com/ilan/emlak-konut-satilik-b.duzu-metrobuse-5dk-125m2-tertemiz-daire-iskanli-yeni-yapi-654470042/detay', { waitUntil: 'networkidle2' });
+
+  const links = await page.evaluate(() => {
+    const featureArticle = document
+      .evaluate(
+        '//*[@id="classifiedDetail"]/div[1]/div[1]/h1',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      )
+      .singleNodeValue;
+
+    return featureArticle.textContent;
+  });
+  console.log(links);
+
+  browser.close();
+
+})();
--- google.js
+++ google.js
@@ -11,7 +11,7 @@ const puppeteer = require('puppeteer');
   });
 
   const page = await browser.newPage();
-  
+
   await page.goto('https://developers.google.com/web/');
 
   // Type into search box.
@@ -25,33 +25,6 @@ const puppeteer = require('puppeteer');
   // Wait for the results page to load and display the results.
   const resultsSelector = '.gsc-results .gsc-thumbnail-inside a.gs-title';
   await page.waitForSelector(resultsSelector);
-
-  // Extract the results from the page.
-  const links = await page.evaluate(resultsSelector => {
-    const anchors = Array.from(document.querySelectorAll(resultsSelector));
-    return anchors.map(anchor => {
-      const title = anchor.textContent.split('|')[0].trim();
-      return `${title} - ${anchor.href}`;
-    });
-  }, resultsSelector);
-  console.log(links.join('\n'));
-
-  // await page.goto('https://www.sahibinden.com/ilan/emlak-konut-satilik-b.duzu-metrobuse-5dk-125m2-tertemiz-daire-iskanli-yeni-yapi-654470042/detay', { waitUntil: 'networkidle2' });
-
-  // const links = await page.evaluate(() => {
-  //   const featureArticle = document
-  //     .evaluate(
-  //       '//*[@id="classifiedDetail"]/div[1]/div[1]/h1',
-  //       document,
-  //       null,
-  //       XPathResult.FIRST_ORDERED_NODE_TYPE,
-  //       null
-  //     )
-  //     .singleNodeValue;
-
-  //   return featureArticle.textContent;
-  // });
-  // console.log(links);
 
   browser.close();
 
